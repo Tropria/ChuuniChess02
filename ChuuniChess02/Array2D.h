de22cf854fe1af8c9ede8eddd9fe369f4de52b62
Tropria <NeoTropria@gmail.com>
2020-11-18 16:36:41 +0800

    Array2D.h & State.h

---
+++ ChuuniChess02/Array2D.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef INCLUDED_ARRAY_2D_H
+#define INCLUDED_ARRAY_2D_H
+
+template<typename T> class Array2D {
+public:
+	Array2D() :mArray(0) {}
+	~Array2D() {
+		if (mArray) {
+			delete[] mArray;
+			mArray = 0;
+		}
+	}
+
+	void setSize(int size0, int size1) {
+		if (mArray) {
+			delete[] mArray; mArray = 0;
+		}
+		mSize0 = size0;
+		mSize1 = size1;
+		mArray = new T[mSize0 * mSize1];
+	}
+	T& operator() (int index0, int index1) {
+		return mArray[index1 * mSize0 + index0];
+	}
+
+	const T& operator() (int index0, int index1) const {
+		return mArray[index1 * mSize0 + index0];
+	}
+
+
+private:
+	T* mArray;
+	int mSize0;
+	int mSize1;
+};
+
+#endif
---
+++ ChuuniChess02/State.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef INCLUDED_STATE_H
+#define INCLUDED_STATE_H
+
+#define GAME_GRID_SIZE (32)
+
+#include "Array2D.h"
+
+class Image;
+
+class State {
+public:
+	State(const char* stageData, int size);
+	void update(int dx, int dy);
+	void draw() const;
+
+private:
+	class Object;
+	void setObjects(const char* stageData, int size);
+
+	Array2D<Object> mObjects;
+
+	int mWidth;//µØÍ¼³ß´ç
+	int mHeight;
+	Image* mImage;
+
+	int mMoveCount;
+};
+
+#endif
